172c63273d5f2f5a321c47ab7e2464852ac8fadb
Joshua Kaiser <joshua.b.kaiser@gmail.com>
2019-09-18 10:46:04 -0500

    test that the user data is coming from mongo

---
+++ example/db/models/user.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const userSchema = require('../schema/userSchema');
+
+const userModel = mongoose.model('User', userSchema);
+
+module.exports = userModel;
