8ebfb104e66ba1b39bf049d0a8a2f4a6ecedcf0d
Joshua Kaiser <joshua.b.kaiser@gmail.com>
2019-09-17 21:35:22 -0500

    add failing header test

--- example/__tests__/integration/user.test.js
+++ example/__tests__/integration/user.test.js
@@ -7,5 +7,7 @@ describe('/users/:email', () => {
   it('returns user objects', async () => {
     const response = await request(app).get('/users/jack.torrance@overlook.com');
     expect(response.status).toBe(200);
+    const { header } = response;
+    expect(header['content-type']).toBe('application/vnd.api+json');
   });
 });
