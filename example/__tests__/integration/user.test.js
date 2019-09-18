c56be3b2088c84154b98cf3012b58950558c03a8
Joshua Kaiser <joshua.b.kaiser@gmail.com>
2019-09-18 08:26:09 -0500

    return mock user object

--- example/__tests__/integration/user.test.js
+++ example/__tests__/integration/user.test.js
@@ -8,6 +8,16 @@ describe('/users/:email', () => {
     const response = await request(app).get('/users/jack.torrance@overlook.com');
     expect(response.status).toBe(200);
     const { header } = response;
-    expect(header['content-type']).toBe('application/vnd.api+json');
+    expect(header['content-type']).toBe('application/vnd.api+json; charset=utf-8');
+
+    expect(JSON.parse(response.text)).toEqual({
+      id: 'jack.torrance@overlook.com',
+      type: 'user',
+      data: {
+        attributes: {
+          firstName: 'Jack',
+        }
+      },
+    });
   });
 });
